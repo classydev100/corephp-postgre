4be7b752a4b3cbef5344f02c43313b65c3a0f017
Mauro Dev <letalwarframe@gmail.com>
2021-04-04 09:50:05 +0200

    replace external link

--- _cadastro/views/index.php
+++ _cadastro/views/index.php
@@ -10,9 +10,11 @@
 		<link rel="icon" href="../../_cadastro/assets/favicon.ico" />
 		<!-- STYLE CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!--		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+        <script src="../../_cadastro/assets/js/jquery-3.5.1.min.js"></script>
 		<link rel="stylesheet" href="../../_cadastro/assets/assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!--		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>-->
+        <script src="../../_cadastro/assets/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" href="../../_cadastro/assets/css/style_index.css">
 		
 		
@@ -161,9 +163,9 @@
 		<script src="../../_cadastro/assets/js/jquery.steps.js"></script>
 
 		<script src="../../_cadastro/assets/js/main.js"></script>
-		<script src="../../_cadastro/assets/js/maskcustom.js" type="text/javascript"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
-        <script src="https://unpkg.com/imask"></script>
+<!--		<script src="../../_cadastro/assets/js/maskcustom.js" type="text/javascript"></script>-->
+<!--        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>-->
+<!--        <script src="https://unpkg.com/imask"></script>-->
         <script src="../../_cadastro/assets/js/vanilla-masker.js"></script>
         <script>
             $(document).ready(function() {
